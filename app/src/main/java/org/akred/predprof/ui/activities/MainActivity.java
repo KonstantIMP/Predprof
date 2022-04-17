822895cf8b430f83c2cd578a952e1a72055be5d0
Ruletka <56>
2022-04-17 11:43:54 +0300

    AsyncNetworkRequest

--- app/src/main/java/org/akred/predprof/ui/activities/MainActivity.java
+++ app/src/main/java/org/akred/predprof/ui/activities/MainActivity.java
@@ -1,13 +1,21 @@
 package org.akred.predprof.ui.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import org.akred.predprof.databinding.ActivityMainBinding;
+import org.akred.predprof.models.DataViewModel;
 import org.akred.predprof.network.DataClient;
+import org.akred.predprof.serialization.Anomaly;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private DataViewModel dataViewModel;
     private ActivityMainBinding binding;
 
     @Override
@@ -17,13 +25,14 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        Thread thread = new Thread(new Runnable() {
+        dataViewModel = new ViewModelProvider(this).get(DataViewModel.class);
+        dataViewModel.getDataFromServer(this);
+
+        dataViewModel.getAnomalies().observe(this, new Observer<List<Anomaly>>() {
             @Override
-            public void run() {
-                DataClient cl = new DataClient();
-                cl.getData();
+            public void onChanged(List<Anomaly> anomalies) {
+                Log.e("Woof", String.valueOf(anomalies.size()));
             }
         });
-        thread.start();
     }
 }
