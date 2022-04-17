b19de98b37031e3de798e1daa0a4870f51e4e7e7
DimeraCejz <you@example.com>
2022-04-17 11:44:45 +0300

    ui

--- app/src/main/java/org/akred/predprof/ui/activities/MainActivity.java
+++ app/src/main/java/org/akred/predprof/ui/activities/MainActivity.java
@@ -2,8 +2,16 @@ package org.akred.predprof.ui.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
 
+import com.davemorrissey.labs.subscaleview.ImageSource;
+
+import org.akred.predprof.R;
 import org.akred.predprof.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,5 +23,9 @@ public class MainActivity extends AppCompatActivity {
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        binding.imageView.setImage(ImageSource.resource(R.drawable.map));
+
+
     }
 }
