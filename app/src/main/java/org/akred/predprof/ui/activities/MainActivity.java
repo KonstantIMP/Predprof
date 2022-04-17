6f56f3fb450d4458194046315750c6338325bf65
DimeraCejz <you@example.com>
2022-04-17 12:10:47 +0300

    ui

--- app/src/main/java/org/akred/predprof/ui/activities/MainActivity.java
+++ app/src/main/java/org/akred/predprof/ui/activities/MainActivity.java
@@ -1,24 +1,24 @@
 package org.akred.predprof.ui.activities;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
-import android.view.MotionEvent;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.davemorrissey.labs.subscaleview.ImageSource;
 
 import org.akred.predprof.R;
 import org.akred.predprof.databinding.ActivityMainBinding;
 import org.akred.predprof.models.DataViewModel;
-import org.akred.predprof.network.DataClient;
 import org.akred.predprof.serialization.Anomaly;
 
 import java.util.List;
@@ -26,7 +26,7 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
     private DataViewModel dataViewModel;
     private ActivityMainBinding binding;
-
+    private double startx, starty, endx, endy;
     private Bitmap imgRes = null;
 
     @Override
@@ -50,6 +50,139 @@ public class MainActivity extends AppCompatActivity {
 
         binding.imageView.setImage(ImageSource.resource(R.drawable.map));
 
+        binding.start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                showStartDialog();
+
+            }
+        });
+
+        binding.end.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                showEndDialog();
+
+            }
+        });
+
 
     }
+
+    private void showEndDialog(){
+
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+        LayoutInflater layoutInflater = getLayoutInflater();
+        final View dialogView = layoutInflater.inflate(R.layout.coordinates_dialog, null);
+        dialogBuilder.setView(dialogView);
+        dialogBuilder.setTitle("Введите конец");
+
+        final Button set = dialogView.findViewById(R.id.set);
+        final Button close = dialogView.findViewById(R.id.close);
+        final EditText x = dialogView.findViewById(R.id.x);
+        final EditText y = dialogView.findViewById(R.id.y);
+
+        final AlertDialog b = dialogBuilder.create();
+        b.setCancelable(false);
+        b.show();
+
+        set.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if(!TextUtils.isEmpty(x.getText().toString())){
+
+                    if(!TextUtils.isEmpty(y.getText().toString())){
+
+                        endx = Double.parseDouble(x.getText().toString());
+                        endy = Double.parseDouble(y.getText().toString());
+                        b.dismiss();
+
+                    } else{
+
+                        y.setError("Введите Y");
+                        y.requestFocus();
+
+                    }
+
+                } else {
+
+                    x.setError("Введите X");
+                    x.requestFocus();
+
+                }
+
+            }
+        });
+
+        close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                b.dismiss();
+
+            }
+        });
+
+    }
+
+    private void showStartDialog(){
+
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+        LayoutInflater layoutInflater = getLayoutInflater();
+        final View dialogView = layoutInflater.inflate(R.layout.coordinates_dialog, null);
+        dialogBuilder.setView(dialogView);
+        dialogBuilder.setTitle("Введите начало");
+
+        final Button set = dialogView.findViewById(R.id.set);
+        final Button close = dialogView.findViewById(R.id.close);
+        final EditText x = dialogView.findViewById(R.id.x);
+        final EditText y = dialogView.findViewById(R.id.y);
+
+        final AlertDialog b = dialogBuilder.create();
+        b.setCancelable(false);
+        b.show();
+
+        set.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if(!TextUtils.isEmpty(x.getText().toString())){
+
+                    if(!TextUtils.isEmpty(y.getText().toString())){
+
+                        startx = Double.parseDouble(x.getText().toString());
+                        starty = Double.parseDouble(y.getText().toString());
+                        b.dismiss();
+
+                    } else{
+
+                        y.setError("Введите Y");
+                        y.requestFocus();
+
+                    }
+
+                } else {
+
+                    x.setError("Введите X");
+                    x.requestFocus();
+
+                }
+
+            }
+        });
+
+        close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                b.dismiss();
+
+            }
+        });
+
+    }
+
 }
